3ebb8c66a7d357824920dd28809ec7c179b71e65
Aastha Shukla <shukla2005aastha@gmail.com>
2026-07-24 21:34:08 +0530

    requirement.txt added

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,12 @@
 "Hello, FASTAPI"
+
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Hospital Appointment Management API",
+    version="1.0.0"
+)
+
+@app.get("/")
+def root():
+    return {"message": "Hospital Appointment Management System API"}
